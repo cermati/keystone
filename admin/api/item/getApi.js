6e55cbbf4984a4bbb83cad386f39f070ac1118a8
theo-pratama <tpratama@cermati.com>
2017-06-19 15:57:25 +0700

    bug fix error get response data

--- admin/api/item/getApi.js
+++ admin/api/item/getApi.js
@@ -6,24 +6,31 @@ var superagent = Bluebird.promisifyAll(require ('superagent'));
 var keystone = require('keystone');
 
 module.exports = function(req, res) {
-	
 	var fields = req.query.fields;
-	
+
 	if (req.query.basic !== undefined) {
 		fields = false;
 	}
-	
+
 	var apiDetails = req.list.options.apiDetails.read;
 	var endpoint = apiDetails.endpoint + '/' + req.params.id;
 
 	superagent
 		.getAsync(endpoint)
-		.then (function(res) {
-			var item = res.body;
+		.then(function(result) {
+			var responseData = {
+				data: result.body,
+				raw: {
+					operation: 'item'
+				}
+			}
+			return req.list.options.apiDetails.read.getResponseData(responseData);
+		})
+		.then(function(item) {
 			item.get = function(colName) {
 				return this[colName];
 			}
-			
+
 			var tasks = [];
 			var drilldown = {};
 			var relationships = {};
@@ -35,20 +42,17 @@ module.exports = function(req, res) {
 						detail: err
 					});
 				}
-				
+
 				res.json(_.assign(req.list.getData(item, fields), {
 					relationships: relationships
 				}));
 
 			});
-			
+
 			return;
-			
+
 		})
-		.catch(function(err){
+		.catch(function(err) {
 			console.log(err.stack || err);
 		});
-		
-
-	
 };
